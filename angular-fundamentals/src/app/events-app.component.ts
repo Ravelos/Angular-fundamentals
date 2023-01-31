5746a3109b88e1b70333e22c0237ea24b5aebd47
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-30 21:12:49 -0300

    modifies the app component

---
+++ angular-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<h1>Events app</h1>'
+})
+export class EventsAppComponent {
+  title = 'angular-fundamentals';
+}
