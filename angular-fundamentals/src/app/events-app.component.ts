607b4b6176409a811bd8b13010eb910c7bbd9510
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-30 21:35:53 -0300

    adds the shield image to the component

--- angular-fundamentals/src/app/events-app.component.ts
+++ angular-fundamentals/src/app/events-app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h1>Events app</h1>'
+  template: `<h1>Events app</h1>
+   <img src="/assets/images/basic-shield.png">`
 })
 export class EventsAppComponent {
   title = 'angular-fundamentals';
