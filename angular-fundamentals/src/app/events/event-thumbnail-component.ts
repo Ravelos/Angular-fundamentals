dbdc670c45b9feb7439f26ee93e1f590f5cf2419
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-07 20:15:10 -0300

    creates logFoo method

--- angular-fundamentals/src/app/events/event-thumbnail-component.ts
+++ angular-fundamentals/src/app/events/event-thumbnail-component.ts
@@ -2,14 +2,14 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
   selector: 'event-thumbnail',
-  templateUrl: './event-thumbnail-component.html'
+  templateUrl: './event-thumbnail-component.html',
 })
-
 export class EventThumbnailComponent {
-   @Input() event:any;
-   @Output() eventClick = new EventEmitter()
+  @Input() event: any;
 
-   handleClickMe(){
-    this.eventClick.emit(this.event.name);
-   }
+  someProperty: any = 'some value';
+
+  logFoo() {
+    console.log('Foo');
   }
+}
