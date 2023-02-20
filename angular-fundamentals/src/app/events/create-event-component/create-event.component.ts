bc07a492760bbd03d0b891097589583a52798278
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-20 11:08:27 -0300

    creates a new folder for this component

---
+++ angular-fundamentals/src/app/events/create-event-component/create-event.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'create-event',
+  templateUrl: './create-event.component.html',
+})
+export class CreateEventComponent {
+  constructor(private router: Router) {}
+  cancel() {
+    this.router.navigate(['/events']);
+  }
+}
