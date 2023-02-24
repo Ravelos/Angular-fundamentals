46a9ab70ea6c853c4519c92f064baaca725abedb
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-24 18:02:56 -0300

    Includes canDeactivate directive in create event component

--- angular-fundamentals/src/routes.ts
+++ angular-fundamentals/src/routes.ts
@@ -6,7 +6,11 @@ import { EventRouteActivator } from './app/events/event-details/event-route-acti
 import { EventsListComponent } from './app/events/event-list-component/events-list-component';
 
 export const appRoutes: Routes = [
-  { path: 'events/new', component: CreateEventComponent },
+  {
+    path: 'events/new',
+    component: CreateEventComponent,
+    canDeactivate: ['canDeactivateCreateEvent'],
+  },
   { path: 'events', component: EventsListComponent },
   {
     path: 'events/:id',
