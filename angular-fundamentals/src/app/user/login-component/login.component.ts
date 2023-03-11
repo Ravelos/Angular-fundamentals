83ce82aae72a08ec6b140ffac9435c1b25393fb0
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-11 18:48:45 -0300

    adds style url

--- angular-fundamentals/src/app/user/login-component/login.component.ts
+++ angular-fundamentals/src/app/user/login-component/login.component.ts
@@ -3,7 +3,8 @@ import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
-  templateUrl: './login.component.html',
+  templateUrl:'./login.component.html',
+  styleUrls:['./login.component.css']
 })
 export class LoginComponent {
   userName: string = '';
