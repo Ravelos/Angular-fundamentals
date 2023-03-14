dc0722d7c55fdac4c27a70751658def778668792
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-13 21:21:45 -0300

    adds reactive forms

--- angular-fundamentals/src/app/user/user.module.ts
+++ angular-fundamentals/src/app/user/user.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { userRoutes } from './user.routes';
 import { LoginComponent } from './login-component/login.component';
-import { ProfileComponent } from './profile.component';
-import { FormsModule } from '@angular/forms';
+import { ProfileComponent } from './profile-component/profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from './auth.service';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(userRoutes), FormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(userRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   declarations: [LoginComponent, ProfileComponent],
   providers: [AuthService],
 })
