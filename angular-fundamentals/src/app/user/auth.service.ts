3e1386f71e0f5cb80b3e5ffe851d93910332b2a3
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-03-13 21:19:49 -0300

    creates updateCurrentUser method

--- angular-fundamentals/src/app/user/auth.service.ts
+++ angular-fundamentals/src/app/user/auth.service.ts
@@ -1,21 +1,25 @@
-import { Injectable } from "@angular/core";
-import { IUser } from "./user.model";
+import { Injectable } from '@angular/core';
+import { IUser } from './user.model';
 
 @Injectable()
+export class AuthService {
+  currentUser: IUser | undefined;
 
-export class AuthService{
-  currentUser:IUser | undefined;
-
-  loginUser(userName:string, password:string){
-    this.currentUser ={
-      id:1,
-      userName:userName,
-      firstName:'John',
-      lastName:'Papa'
-    }
+  loginUser(userName: string, password: string) {
+    this.currentUser = {
+      id: 1,
+      userName: userName,
+      firstName: 'John',
+      lastName: 'Papa',
+    };
   }
 
-  isAuthenticated(){
+  isAuthenticated() {
     return !!this.currentUser;
+  }
+
+  updateCurrentUser(firstName: string, lastName: string) {
+    this.currentUser.firstName = firstName;
+    this.currentUser.lastName = lastName;
   }
 }
